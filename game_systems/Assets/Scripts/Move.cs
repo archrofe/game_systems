47af36e74cb912c4a55c6c76027c2c1c43efed70
archrofe <30814733+archrofe@users.noreply.github.com>
2018-07-30 16:02:07 +1000

    Basic Networking

---
+++ game_systems/Assets/Scripts/Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody rigid;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+        Vector3 force = new Vector3(inputH, 0, inputV);
+        rigid.AddForce(force * speed);
+    }
+}
